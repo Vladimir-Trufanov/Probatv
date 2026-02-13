5780255a95613b4a78e16294771cde9279428d9d
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2026-02-13 09:10:52 +0300

    Просмотр кода

--- www/Websocket/wsTve/WebSocketServer.php
+++ www/Websocket/wsTve/WebSocketServer.php
@@ -122,6 +122,25 @@ class WebSocketServer
   // **************************************************************************
   // *                         Отправить сообщение клиенту                    *
   // **************************************************************************
+  
+  // Ключевое слово static в PHP используется для объявления функций, которые принадлежат 
+  // самому классу, а не экземплярам класса. Это позволяет:
+  // - получить доступ к функции без создания экземпляра класса. Например, вместо того 
+  // чтобы писать: $object = new MyClass(); $object.someMethod(), можно написать: 
+  // MyClass::someStaticMethod();
+  // - реализовать функционал, не привязанный к конкретному экземпляру. Логику, которая 
+  // может быть общей для нескольких экземпляров класса, можно извлечь и поместить 
+  // внутрь статического метода;
+  // - использовать статические методы для доступа к приватным статическим свойствам. 
+  // Например, геттеры и сеттеры, которые нужны редко, но всё же бывают нужны.
+  // Важно: статические методы не принадлежат объектам, они — часть класса, поэтому 
+  // из них невозможно получить доступ к объекту через $this — внутри статического 
+  // метода $this не существует. 
+  // Синтаксис: чтобы объявить статическую функцию в PHP, перед её именем ставится 
+  // модификатор static. Например: public static function test(). При обращении к 
+  // статическим функциям используется имя класса и оператор ::, вместо операции доступа ->. 
+  // Например: MyClass::test(). 
+
   public static function response($connect, $data) 
   {
     // $connect (обязательный параметр) — ресурс сокета, полученный из socket_create или socket_accept;
@@ -362,213 +381,229 @@ class WebSocketServer
       }
     }
   }
-
-    /**
-     * Для кодирования сообщений перед отправкой клиенту
-     */
-    private static function encode($payload, $type = 'text', $masked = false) {
-        $frameHead = array();
-        $payloadLength = strlen($payload);
-
-        switch ($type) {
-            case 'text':
-                // first byte indicates FIN, Text-Frame (10000001):
-                $frameHead[0] = 129;
-                break;
-            case 'close':
-                // first byte indicates FIN, Close Frame(10001000):
-                $frameHead[0] = 136;
-                break;
-            case 'ping':
-                // first byte indicates FIN, Ping frame (10001001):
-                $frameHead[0] = 137;
-                break;
-            case 'pong':
-                // first byte indicates FIN, Pong frame (10001010):
-                $frameHead[0] = 138;
-                break;
-        }
-
-        // set mask and payload length (using 1, 3 or 9 bytes)
-        if ($payloadLength > 65535) {
-            $payloadLengthBin = str_split(sprintf('%064b', $payloadLength), 8);
-            $frameHead[1] = ($masked === true) ? 255 : 127;
-            for ($i = 0; $i < 8; $i++) {
-                $frameHead[$i + 2] = bindec($payloadLengthBin[$i]);
-            }
-            // most significant bit MUST be 0
-            if ($frameHead[2] > 127) {
-                return array('type' => '', 'payload' => '', 'error' => 'frame too large (1004)');
-            }
-        } elseif ($payloadLength > 125) {
-            $payloadLengthBin = str_split(sprintf('%016b', $payloadLength), 8);
-            $frameHead[1] = ($masked === true) ? 254 : 126;
-            $frameHead[2] = bindec($payloadLengthBin[0]);
-            $frameHead[3] = bindec($payloadLengthBin[1]);
-        } else {
-            $frameHead[1] = ($masked === true) ? $payloadLength + 128 : $payloadLength;
-        }
-
-        // convert frame-head to string:
-        foreach (array_keys($frameHead) as $i) {
-            $frameHead[$i] = chr($frameHead[$i]);
-        }
-        if ($masked === true) {
-            // generate a random mask:
-            $mask = array();
-            for ($i = 0; $i < 4; $i++) {
-                $mask[$i] = chr(rand(0, 255));
-            }
-            $frameHead = array_merge($frameHead, $mask);
-        }
-        $frame = implode('', $frameHead);
-
-        // append payload to frame:
-        for ($i = 0; $i < $payloadLength; $i++) {
-            $frame .= ($masked === true) ? $payload[$i] ^ $mask[$i % 4] : $payload[$i];
-        }
-
-        return $frame;
+  // **************************************************************************
+  // *              Закодировать сообщение перед отправкой клиенту            *
+  // **************************************************************************
+  private static function encode($payload, $type = 'text', $masked = false) 
+  {
+    $frameHead = array();
+    // Определяем длину входного сообщения
+    $payloadLength = strlen($payload);
+    switch ($type) 
+    {
+      case 'text':
+        // Первый байт указывает на FIN, Text-Frame (10000001):
+        $frameHead[0] = 129;
+        break;
+      case 'close':
+        // first byte indicates FIN, Close Frame(10001000):
+        $frameHead[0] = 136;
+        break;
+      case 'ping':
+        // first byte indicates FIN, Ping frame (10001001):
+        $frameHead[0] = 137;
+        break;
+      case 'pong':
+        // first byte indicates FIN, Pong frame (10001010):
+        $frameHead[0] = 138;
+        break;
     }
-
-    /**
-     * Для декодирования сообщений, полученных от клиента
-     */
-    private static function decode($data) {
-        if ( ! strlen($data)) {
-            return false;
-        }
-
-        $unmaskedPayload = '';
-        $decodedData = array();
-
-        // estimate frame type:
-        $firstByteBinary = sprintf('%08b', ord($data[0]));
-        $secondByteBinary = sprintf('%08b', ord($data[1]));
-        $opcode = bindec(substr($firstByteBinary, 4, 4));
-        $isMasked = ($secondByteBinary[0] == '1') ? true : false;
-        $payloadLength = ord($data[1]) & 127;
-
-        // unmasked frame is received:
-        if (!$isMasked) {
-            return array('type' => '', 'payload' => '', 'error' => 'protocol error (1002)');
-        }
-
-        switch ($opcode) {
-            // text frame:
-            case 1:
-                $decodedData['type'] = 'text';
-                break;
-            case 2:
-                $decodedData['type'] = 'binary';
-                break;
-            // connection close frame:
-            case 8:
-                $decodedData['type'] = 'close';
-                break;
-            // ping frame:
-            case 9:
-                $decodedData['type'] = 'ping';
-                break;
-            // pong frame:
-            case 10:
-                $decodedData['type'] = 'pong';
-                break;
-            default:
-                return array('type' => '', 'payload' => '', 'error' => 'unknown opcode (1003)');
-        }
-
-        if ($payloadLength === 126) {
-            $mask = substr($data, 4, 4);
-            $payloadOffset = 8;
-            $dataLength = bindec(sprintf('%08b', ord($data[2])) . sprintf('%08b', ord($data[3]))) + $payloadOffset;
-        } elseif ($payloadLength === 127) {
-            $mask = substr($data, 10, 4);
-            $payloadOffset = 14;
-            $tmp = '';
-            for ($i = 0; $i < 8; $i++) {
-                $tmp .= sprintf('%08b', ord($data[$i + 2]));
-            }
-            $dataLength = bindec($tmp) + $payloadOffset;
-            unset($tmp);
-        } else {
-            $mask = substr($data, 2, 4);
-            $payloadOffset = 6;
-            $dataLength = $payloadLength + $payloadOffset;
-        }
-
-        /**
-         * We have to check for large frames here. socket_recv cuts at 1024 bytes
-         * so if websocket-frame is > 1024 bytes we have to wait until whole
-         * data is transferd.
-         */
-        if (strlen($data) < $dataLength) {
-            return false;
-        }
-
-        if ($isMasked) {
-            for ($i = $payloadOffset; $i < $dataLength; $i++) {
-                $j = $i - $payloadOffset;
-                if (isset($data[$i])) {
-                    $unmaskedPayload .= $data[$i] ^ $mask[$j % 4];
-                }
-            }
-            $decodedData['payload'] = $unmaskedPayload;
-        } else {
-            $payloadOffset = $payloadOffset - 4;
-            $decodedData['payload'] = substr($data, $payloadOffset);
-        }
-
-        return $decodedData;
+    // Устанавливаем маску и длину "полезной нагрузки" (используя 1, 3 или 9 байт)
+    if ($payloadLength > 65535) 
+    {
+      // Представляем длину в двоичном виде, как строку. Дополняем нулями слева и 
+      // разбивает её на части, как масиив строк по 8 символов
+      $payloadLengthBin = str_split(sprintf('%064b', $payloadLength), 8);
+      $frameHead[1] = ($masked === true) ? 255 : 127;
+      for ($i = 0; $i < 8; $i++) 
+      {
+        // Преобразование строки из массива, представляющие двоичнык числа, в их десятичный эквивалент
+        $frameHead[$i + 2] = bindec($payloadLengthBin[$i]);
+      }
+      // Если старший бит больше нуля (ДОЛЖЕН быть равен 0), то выдаем ошибку
+      if ($frameHead[2] > 127) 
+      {
+        return array('type' => '', 'payload' => '', 'error' => 'frame too large (1004)');
+      }
+    } 
+    elseif ($payloadLength > 125) 
+    {
+      $payloadLengthBin = str_split(sprintf('%016b', $payloadLength), 8);
+      $frameHead[1] = ($masked === true) ? 254 : 126;
+      $frameHead[2] = bindec($payloadLengthBin[0]);
+      $frameHead[3] = bindec($payloadLengthBin[1]);
+    } 
+    else 
+    {
+      $frameHead[1] = ($masked === true) ? $payloadLength + 128 : $payloadLength;
     }
-
-    /**
-     * «Рукопожатие», т.е. отправка заголовков согласно протоколу WebSocket
-     */
-    private function handshake($connect) {
-
-        $info = array();
-
-        $data = socket_read($connect, 1000);
-        $lines = explode("\r\n", $data);
-        foreach ($lines as $i => $line) {
-            if ($i) {
-                if (preg_match('/\A(\S+): (.*)\z/', $line, $matches)) {
-                    $info[$matches[1]] = $matches[2];
-                }
-            } else {
-                $header = explode(' ', $line);
-                $info['method'] = $header[0];
-                $info['uri'] = $header[1];
-            }
-            if (empty(trim($line))) break;
-        }
-
-        // получаем адрес клиента
-        $ip = $port = null;
-        if ( ! socket_getpeername($connect, $ip, $port)) {
-            return false;
-        }
-        $info['ip'] = $ip;
-        $info['port'] = $port;
-
-        if (empty($info['Sec-WebSocket-Key'])) {
-            return false;
-        }
-
-        // отправляем заголовок согласно протоколу вебсокета
-        $SecWebSocketAccept = 
-            base64_encode(pack('H*', sha1($info['Sec-WebSocket-Key'] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
-        $upgrade = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-                   "Upgrade: websocket\r\n" .
-                   "Connection: Upgrade\r\n" .
-                   "Sec-WebSocket-Accept:".$SecWebSocketAccept."\r\n\r\n";
-        socket_write($connect, $upgrade);
-
-        return true;
-
+    // Преобразовываем заголовок фрагмента в строку
+    foreach (array_keys($frameHead) as $i) 
+    {
+      $frameHead[$i] = chr($frameHead[$i]);
     }
-
+    if ($masked === true) 
+    {
+      // Генерируем случайную маску
+      $mask = array();
+      for ($i = 0; $i < 4; $i++) 
+      {
+        $mask[$i] = chr(rand(0, 255));
+      }
+      $frameHead = array_merge($frameHead, $mask);
+    }
+    $frame = implode('', $frameHead);
+    // Добавляем полезную нагрузку к фрагменту
+    for ($i = 0; $i < $payloadLength; $i++) 
+    {
+      $frame .= ($masked === true) ? $payload[$i] ^ $mask[$i % 4] : $payload[$i];
+    }
+    return $frame;
+  }
+  // **************************************************************************
+  // *               Декодировать сообщение, полученное от клиента            *
+  // **************************************************************************
+  private static function decode($data) 
+  {
+    if (!strlen($data)) 
+    {
+      return false;
+    }
+    $unmaskedPayload = '';
+    $decodedData = array();
+    // Оцениваем тип кадра
+    $firstByteBinary = sprintf('%08b', ord($data[0]));
+    $secondByteBinary = sprintf('%08b', ord($data[1]));
+    $opcode = bindec(substr($firstByteBinary, 4, 4));
+    $isMasked = ($secondByteBinary[0] == '1') ? true : false;
+    $payloadLength = ord($data[1]) & 127;
+    // Возвращаем ошибку, если получен немаскированный кадр
+    if (!$isMasked) 
+    {
+      return array('type' => '', 'payload' => '', 'error' => 'protocol error (1002)');
+    }
+    switch ($opcode) 
+    {
+      // text frame:
+      case 1:
+        $decodedData['type'] = 'text';
+        break;
+      case 2:
+        $decodedData['type'] = 'binary';
+        break;
+      // connection close frame:
+      case 8:
+        $decodedData['type'] = 'close';
+        break;
+      // ping frame:
+      case 9:
+        $decodedData['type'] = 'ping';
+        break;
+      // pong frame:
+      case 10:
+        $decodedData['type'] = 'pong';
+        break;
+      default:
+        return array('type' => '', 'payload' => '', 'error' => 'unknown opcode (1003)');
+    }
+    if ($payloadLength === 126) 
+    {
+      $mask = substr($data, 4, 4);
+      $payloadOffset = 8;
+      $dataLength = bindec(sprintf('%08b', ord($data[2])) . sprintf('%08b', ord($data[3]))) + $payloadOffset;
+    } 
+    elseif ($payloadLength === 127) 
+    {
+      $mask = substr($data, 10, 4);
+      $payloadOffset = 14;
+      $tmp = '';
+      for ($i = 0; $i < 8; $i++) 
+      {
+        $tmp .= sprintf('%08b', ord($data[$i + 2]));
+      }
+      $dataLength = bindec($tmp) + $payloadOffset;
+      unset($tmp);
+    } 
+    else 
+    {
+      $mask = substr($data, 2, 4);
+      $payloadOffset = 6;
+      $dataLength = $payloadLength + $payloadOffset;
+    }
+    // Здесь мы должны проверить, является ли фрейм большим. socket_recv ограничен 
+    // до 1024 байт, поэтому, если websocket-фрейм > 1024 байт, нам нужно подождать,
+    // пока не будут переданы все данные.
+    if (strlen($data) < $dataLength) 
+    {
+      return false;
+    }
+    if ($isMasked) 
+    {
+      for ($i = $payloadOffset; $i < $dataLength; $i++) 
+      {
+        $j = $i - $payloadOffset;
+        if (isset($data[$i])) 
+        {
+          $unmaskedPayload .= $data[$i] ^ $mask[$j % 4];
+        }
+      }
+      $decodedData['payload'] = $unmaskedPayload;
+    } 
+    else 
+    {
+      $payloadOffset = $payloadOffset - 4;
+      $decodedData['payload'] = substr($data, $payloadOffset);
+    }
+    return $decodedData;
+  }
+  // **************************************************************************
+  // *  «Рукопожатие», т.е. отправить заголовки согласно протоколу WebSocket  *
+  // **************************************************************************
+  private function handshake($connect) 
+  {
+    $info = array();
+    $data = socket_read($connect, 1000);
+    $lines = explode("\r\n", $data);
+    foreach ($lines as $i => $line) 
+    {
+      if ($i) 
+      {
+        if (preg_match('/\A(\S+): (.*)\z/', $line, $matches)) 
+        {
+          $info[$matches[1]] = $matches[2];
+        }
+      } 
+      else 
+      {
+        $header = explode(' ', $line);
+        $info['method'] = $header[0];
+        $info['uri'] = $header[1];
+      }
+      if (empty(trim($line))) break;
+    }
+    // Получаем адрес клиента
+    $ip = $port = null;
+    if (!socket_getpeername($connect, $ip, $port)) 
+    {
+      return false;
+    }
+    $info['ip'] = $ip;
+    $info['port'] = $port;
+    if (empty($info['Sec-WebSocket-Key'])) 
+    {
+      return false;
+    }
+    // Отправляем заголовок согласно протоколу вебсокета
+    $SecWebSocketAccept = 
+    base64_encode(pack('H*', sha1($info['Sec-WebSocket-Key'] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
+    $upgrade = 
+      "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
+      "Upgrade: websocket\r\n" .
+      "Connection: Upgrade\r\n" .
+      "Sec-WebSocket-Accept:".$SecWebSocketAccept."\r\n\r\n";
+    socket_write($connect, $upgrade);
+    return true;
+  }
 }
 
 // ********************************************** wsTve/WebSocketServer.php ***
